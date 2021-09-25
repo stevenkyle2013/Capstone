1c5c7734bb3d3b0a348762c50d2791648fdb850a
stevenkyle2013 <stevenkyle2013@gmail.com>
2021-09-24 17:09:03 -0700

    Stock updating portion of script is complete

--- SocialMediaDataUpdatingScript.py
+++ SocialMediaDataUpdatingScript.py
@@ -4,7 +4,8 @@ import numpy as np
 import twint
 from psaw import PushshiftAPI
 import praw
-import datetime as dt
+import datetime
+from datetime import datetime as dt
 import yfinance as yf
 import requests 
 import requests.auth
@@ -35,21 +36,20 @@ if __name__ == "__main__":
     response = input()
     
     if response == 'Y':
-        
-        print('Updating from {} to {}'.format(Latest_date, update_date))
-        
+                
         #Converting time to datetime to add one day and putting back to string 
         Latest_date = dt.strptime(Latest_date, '%Y-%m-%d') #Not sure if strptime is the right one
-        print(type(Latest_date))
-        print(Latest_date)
+        Latest_date += datetime.timedelta(days=1)
         
+        print('Updating from {} to {}'.format(Latest_date, update_date))
+
         # Downloading Roku data from yfinance
-        #stock_data = yf.Ticker('ROKU')
-        #new_stock_data = stock_data.history(start = Latest_date, end = update_date)
+        stock_data = yf.Ticker('ROKU')
+        new_stock_data = stock_data.history(start = Latest_date, end = update_date)
                 
         # updating stock data csv
-        #new_stock_data.to_csv('/Users/stevenkyle/Documents/Flatiron/Capstone/Capstone/StockData_Test.csv', 
-        #                      mode='a', index=True, header=False)
+        new_stock_data.to_csv('/Users/stevenkyle/Documents/Flatiron/Capstone/Capstone/StockData_Test.csv', 
+                              mode='a', index=True, header=False)
          
 
         
