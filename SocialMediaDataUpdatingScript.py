3702c1dd299ea257677887d16a87ae67a64d1035
stevenkyle2013 <stevenkyle2013@gmail.com>
2021-09-25 17:16:37 -0700

    Troubleshooting Twitter portion of script

--- SocialMediaDataUpdatingScript.py
+++ SocialMediaDataUpdatingScript.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
                 
         # updating stock data csv
         new_stock_data.to_csv('/Users/stevenkyle/Documents/Flatiron/Capstone/Capstone/StockData_Test.csv', 
-                              mode='a', index=True, header=False)
+                              mode='a', index=True, header=False, line_terminator=None)
         
         
         
@@ -91,8 +91,8 @@ if __name__ == "__main__":
                                'quote_url','near','geo','source','user_rt_id','user_rt','retweet_id','reply_to',
                                'retweet_date','translate','trans_src','trans_dest'], inplace=True)
     
-            
-            Tweets_df.sort_values(by='nlikes', axis=0, ascending=False, inplace=True)
+            print(Tweets_df.columns)
+            #Tweets_df.sort_values(by='nlikes', axis=0, ascending=False, inplace=True)
     
             # Take the top 5 in dataframe to write
             df_to_write = Tweets_df[0:5]
@@ -104,8 +104,74 @@ if __name__ == "__main__":
             time.sleep(3)
         
         
-        
-        
         # Update Reddit csv
         
-        #print()
+        # Looping throught the dates
+        for i in range(len(stock_days_itter) - 1):
+    
+            r = praw.Reddit(client_id=client_id,
+                            client_secret=client_secret,
+                            user_agent=user_agent)
+
+            api = PushshiftAPI(r)
+    
+            # Setting parameters and doing a search
+            start_epoch = stock_days_itter[i]
+            next_day = i+1
+            end_epoch = stock_days_itter[next_day]
+
+            api_request_generator = api.search_submissions(subreddit=Reddit_subreddits,
+                                                           after = start_epoch, before=end_epoch,
+                                                           q='(ROKU)|(Roku)|(roku)|(#Roku)|(#ROKU)|(#roku)|($ROKU)|($Roku)|($roku)',
+                                                           limit=1000)
+    
+            # Adding search data to a dataframe
+            Reddit_df = pd.DataFrame(columns =['ID','Num_Comments','Score','Subreddit','Title','Upvote_Ratio','Created',
+                                   'Created_utc','Self_text','Date'] )
+            ID=[]
+            Num_Comments=[]
+            Score=[]
+            Subreddit = []
+            Title = []
+            Upvote_Ratio = []
+            Created = []
+            Created_utc = []
+            Self_text = []
+            Date = []
+    
+            for submissions in api_request_generator:
+                ID.append(submissions.id)
+                Num_Comments.append(submissions.num_comments)
+                Score.append(submissions.score)
+                Subreddit.append(submissions.subreddit)
+                Title.append(submissions.title)
+                Upvote_Ratio.append(submissions.upvote_ratio)
+                Created.append(submissions.created)
+                Created_utc.append(submissions.created_utc)
+                Self_text.append(submissions.selftext)
+                Date.append(stock_days[i])
+    
+            temp_df = pd.DataFrame({'ID':ID,
+                            'Num_Comments':Num_Comments,
+                            'Score':Score,
+                            'Subreddit':Subreddit,
+                             'Title':Title,
+                            'Upvote_Ratio':Upvote_Ratio,
+                            'Created':Created,
+                            'Created_utc':Created_utc,
+                            'Self_text':Self_text,
+                            'Date':Date})
+
+            Reddit_df = Reddit_df.append(temp_df)
+            Reddit_df.sort_values(by='Score', axis=0, ascending=False, inplace=True)
+            Reddit_df.drop(Reddit_df[(Reddit_df['Self_text']=='[deleted]')|(Reddit_df['Self_text']=='[removed]')].index, 
+                          inplace=True)
+            Reddit_df.drop(Reddit_df[Reddit_df['Score']<10].index, inplace=True)
+            dataframe_to_write = Reddit_df[0:5]
+    
+            # Writing dataframe to a csv
+            datarame_to_write.to_csv('/Users/stevenkyle/Documents/Flatiron/Capstone/Capstone/redditdata_Test.csv', mode='a',
+                                     header=False, index=False)
+    
+            time.sleep(3)
+        
